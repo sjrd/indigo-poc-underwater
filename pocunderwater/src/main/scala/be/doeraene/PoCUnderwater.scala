7d97ddc4784a2137d31020f5da9f529356e7bb23
Sébastien Doeraene <sjrdoeraene@gmail.com>
2026-04-19 10:38:59 +0200

    Blend shader example.

--- pocunderwater/src/main/scala/be/doeraene/PoCUnderwater.scala
+++ pocunderwater/src/main/scala/be/doeraene/PoCUnderwater.scala
@@ -4,61 +4,142 @@ import be.doeraene.generated.Assets
 import be.doeraene.generated.Config
 import indigo.*
 import indigo.scenes.*
+import ultraviolet.syntax.*
 
+import scala.annotation.nowarn
 import scala.scalajs.js.annotation.JSExportTopLevel
 
 @JSExportTopLevel("IndigoGame")
-object PoCUnderwater extends IndigoGame[BootData, StartUpData, Model, ViewModel]:
+object PoCUnderwater extends IndigoSandbox[Unit, Unit]:
 
-  def initialScene(bootData: BootData): Option[SceneName] =
-    None
+  val config: GameConfig =
+    Config.config.noResize
+      .withMagnification(2)
 
-  def scenes(bootData: BootData): NonEmptyBatch[Scene[StartUpData, Model, ViewModel]] =
-    NonEmptyBatch(GameScene)
+  val assets: Set[AssetType] =
+    Assets.assets.assetSet
 
-  val eventFilters: EventFilters =
-    EventFilters.Permissive
+  val fonts: Set[FontInfo]       = Set()
+  val animations: Set[Animation] = Set()
 
-  def boot(flags: Map[String, String]): Outcome[BootResult[BootData, Model]] =
-    Outcome(
-      BootResult(Config.config, BootData())
-        .withAssets(Assets.assets.assetSet)
+  val shaders: Set[ShaderProgram] =
+    Set(
+      CustomEntityShader.shader,
+      CustomBlendShader.shader
     )
 
-  def initialModel(startupData: StartUpData): Outcome[Model] =
-    Outcome(Model())
+  def setup(assetCollection: AssetCollection, dice: Dice): Outcome[Startup[Unit]] =
+    Outcome(Startup.Success(()))
 
-  def initialViewModel(startupData: StartUpData, model: Model): Outcome[ViewModel] =
-    Outcome(ViewModel())
+  def initialModel(startupData: Unit): Outcome[Unit] =
+    Outcome(())
 
-  def setup(
-      bootData: BootData,
-      assetCollection: AssetCollection,
-      dice: Dice
-  ): Outcome[Startup[StartUpData]] =
-    Outcome(Startup.Success(StartUpData()))
-
-  def updateModel(
-      context: Context[StartUpData],
-      model: Model
-  ): GlobalEvent => Outcome[Model] =
+  def updateModel(context: Context[Unit], model: Unit): GlobalEvent => Outcome[Unit] =
     _ => Outcome(model)
 
-  def updateViewModel(
-      context: Context[StartUpData],
-      model: Model,
-      viewModel: ViewModel
-  ): GlobalEvent => Outcome[ViewModel] =
-    _ => Outcome(viewModel)
+  /** In this scene we render 3 boxes, using 4 layers (mostly for clarity) and 1 blend material.
+    *
+    * Left to right:
+    *
+    *   - Box 1 is a simple graphic with a bitmap texture
+    *   - Box 2 is a custom entity running the same custom shader we used in the basic entity shader
+    *     example.
+    *   - Box 3 is made by rendering Box 1 on one layer, then rendering Box 2 on a layer above it,
+    *     and giving this layer our custom blend material explaining how we'd like it to be merge to
+    *     the layer below.
+    *
+    * In this instance the example shows all the arguments you can supply to a `Blending` instance,
+    * but you can also construct them with only the `BlendMaterial`, or even skip it and replace
+    * `withBlending` with `withBlendMaterial`.
+    */
+  // ``` scala
+  def present(context: Context[Unit], model: Unit): Outcome[SceneUpdateFragment] =
+    val gap = 2
 
-  def present(
-      context: Context[StartUpData],
-      model: Model,
-      viewModel: ViewModel
-  ): Outcome[SceneUpdateFragment] =
-    Outcome(SceneUpdateFragment.empty)
+    Outcome(
+      SceneUpdateFragment(
+        Layer(
+          Graphic(0, 0, 64, 64, Material.Bitmap(Assets.assets.nineslice))
+            .moveTo(10, 10)
+        ),
+        Layer(
+          BlankEntity(0, 0, 64, 64, ShaderData(CustomEntityShader.shader.id))
+            .moveTo(10 + 64 + gap, 10)
+        ),
+        Layer(
+          Graphic(0, 0, 64, 64, Material.Bitmap(Assets.assets.nineslice))
+            .moveTo(10 + 64 + gap + 64 + gap, 10)
+        ),
+        Layer(
+          BlankEntity(0, 0, 64, 64, ShaderData(CustomEntityShader.shader.id))
+            .moveTo(10 + 64 + gap + 64 + gap, 10)
+        ).withBlending(
+          Blending(
+            entity = Blend.Normal,
+            layer = Blend.Normal,
+            blendMaterial = CustomBlendMaterial(),
+            clearColor = None
+          )
+        )
+      )
+    )
+  // ```
 
-final case class BootData()
-final case class StartUpData()
-final case class Model()
-final case class ViewModel()
+object CustomEntityShader:
+
+  val shader: ShaderProgram =
+    UltravioletShader.entityFragment(
+      ShaderId("custom-entity-shader"),
+      EntityShader.fragment[FragmentEnv](fragment, FragmentEnv.reference)
+    )
+
+  @nowarn("msg=unused")
+  inline def fragment: Shader[FragmentEnv, Unit] =
+    Shader[FragmentEnv] { env =>
+      def fragment(color: vec4): vec4 =
+        val col: vec3 = 0.5f + 0.5f * cos(env.TIME + env.UV.xyx + vec3(0.0f, 2.0f, 4.0f))
+        vec4(col, 1.0f)
+    }
+
+/** A `BlendMaterial` is nothing more than a class that provides the `ShaderData` used to tell
+  * Indigo how to merge one layer down onto the layer below it.
+  *
+  * In terms of the process, each layer is first rendered, and then the blending is performed. In
+  * effect, you are combining to images of the same size in the same location.
+  */
+// ``` scala
+final case class CustomBlendMaterial() extends BlendMaterial:
+  def toShaderData: ShaderData =
+    ShaderData(CustomBlendShader.shader.id)
+// ```
+
+/** The structure of a blend shader is _very_ similar that seen in the basic entity shader example.
+  * There are a number of differences though that all boil down to telling Indigo to expect a blend
+  * shader instead of an entity shader.
+  *
+  * In terms of the actual code, the main thing to note is that the `BlendFragmentEnv` has replaced
+  * our usual `FragmentEnv`, and this gives us access to `env.SRC` and `env.DST`, which are the
+  * color values of the current pixel on each layer, where src is merging down to dst.
+  *
+  * `env.SRC` and `env.DST` are convenience variables that you get for free, but you can always
+  * change which pixel you'd like to reference using
+  * `texture2D(SRC_CHANNEL, <some custom UV coordinate)`.
+  *
+  * This shader mixes the two layers together 50-50.
+  */
+// ``` scala
+object CustomBlendShader:
+
+  val shader: ShaderProgram =
+    UltravioletShader.blendFragment(
+      ShaderId("custom-blend-shader"),
+      BlendShader.fragment[BlendFragmentEnv](fragment, BlendFragmentEnv.reference)
+    )
+
+  @nowarn("msg=unused")
+  inline def fragment: Shader[BlendFragmentEnv, Unit] =
+    Shader[BlendFragmentEnv] { env =>
+      def fragment(color: vec4): vec4 =
+        mix(env.DST, env.SRC, 0.5f)
+    }
+// ```
